8df81f1d94bf2754dd0148a59b0a202844233894
vitiima <matheus_famas@hotmail.com>
2025-03-03 16:40:34 -0300

    continuidade das paginas

--- src/app/screens/Agradecimento.tsx
+++ src/app/screens/Agradecimento.tsx
@@ -1,11 +1,71 @@
+import React from "react";
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { Link } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 import Logo from "@/components/logo";
-import { View } from "react-native";
 
-export default function Agradecimento() {
+export default function DocumentosScreen() {
     return (
-        <View>
-            <Logo />
+        <View style={styles.container}>
+            <View style={styles.sessionLogo}>
+                <Logo width={100} height={123}/>
+            </View>
+
+            <Text style={styles.title}>Regras para Foto do Documento</Text>
+
+            <View style={styles.warningContainer}>
+                <Text style={styles.warningText}>Concluido!</Text>
+                <Text style={styles.warningText}>Ja recebemos todas as informaçoes e daremos continuidade no processo</Text>
+                <Text style={styles.warningText}>Obrigado por utilizar nosso serviço</Text>
+            </View>
+
+            <View style={styles.containerBtn}>
+                <Link href="./Logs" asChild>
+                    <Pressable style={styles.btn}>
+                        <AntDesign name="arrowright" size={50} color="white" />
+                    </Pressable>
+                </Link>
+            </View>
         </View>
-        
     );
 }
+
+const styles = StyleSheet.create({
+    container: { 
+        flex: 1,
+        justifyContent: "center",
+        
+        gap: 2,
+        paddingHorizontal: 20,
+    },
+    sessionLogo: {
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 10,
+    },
+    warningContainer: {   
+        gap: 14,
+        paddingHorizontal: 10,
+    },
+    warningText: {
+        color: "#AA1111",
+        fontSize: 28,
+        textAlign: "center",
+    },
+    containerBtn: {
+        marginTop: 20,
+        alignItems: 'flex-end'
+    },
+    btn: {
+        backgroundColor: "#23CF5C",
+        borderRadius: 50,
+        padding: 7,
+        marginRight: 12
+      
+    },
+});
--- src/app/screens/Confirmacao.tsx
+++ src/app/screens/Confirmacao.tsx
@@ -1,10 +1,98 @@
-import Logo from "@/components/logo";
-import { View } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default function Confirmacao() {
-    return (
-        <View>
-            <Logo />
-        </View>
-    );
+export default function Confirmacao({ }) {
+  const fotoClienteUri = route?.params?.fotoClienteUri;
+  const fotoDocumentoUri = route?.params?.fotoDocumentoUri;
+
+  return (
+    <View style={styles.container}>
+
+   
+      <Text style={styles.title}>Coleta do Documento</Text>
+      <View style={styles.box}>
+        {fotoClienteUri ? (
+          <Image
+            source={{ uri: fotoClienteUri }}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        ) : (
+          <Text style={styles.placeholderText}>
+            Nenhuma foto do cliente disponível
+          </Text>
+        )}
+      </View>
+
+     
+      <Text style={styles.title}>Coleta da Biometria</Text>
+      <View style={styles.box}>
+        {fotoDocumentoUri ? (
+          <Image
+            source={{ uri: fotoDocumentoUri }}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        ) : (
+          <Text style={styles.placeholderText}>
+            Nenhuma foto do documento disponível
+          </Text>
+        )}
+      </View>
+
+      <TouchableOpacity style={[styles.button, {marginTop: 20}]}>
+        <Text style={styles.buttonText}>Reiniciar Coleta</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, {marginTop: 10}]}>
+        <Text style={styles.buttonText}>Confirmar e Enviar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  box: {
+    width: '100%',
+    height: 120,
+    backgroundColor: '#E0E0E0',
+    borderRadius: 8,
+    marginBottom: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '95%',
+    height: '95%',
+    borderRadius: 8,
+  },
+  placeholderText: {
+    color: '#777',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  button: {
+    width: '80%',
+    backgroundColor: '#28a745', 
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
