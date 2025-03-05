d20ac4224d049759fe75adef44ac094163b56cc0
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-03-05 16:46:25 -0300

    Update

--- src/database/initializeDatabase.ts
+++ src/database/initializeDatabase.ts
@@ -1,12 +1,17 @@
 import { type SQLiteDatabase } from "expo-sqlite";
 export async function initializeDatabase(db: SQLiteDatabase) {
   await db.execAsync(`
+    DROP TABLE IF EXISTS users;
+`);
+
+  await db.execAsync(`
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name TEXT,
       CPF TEXT,
+      email TEXT,
+      telefone TEXT,
       isDoc BOOLEAN NOT NULL DEFAULT 0,
-      typeDoc TEXT,
       doc TEXT,
       biometria TEXT,
       createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
